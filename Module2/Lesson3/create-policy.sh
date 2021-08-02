e29f319ae876202f8d60b241264cd5220152e222
Arthur Colombini Gusmão <arthurcgusmao@users.noreply.github.com>
2021-08-02 22:23:27 +0200

    Module 2: Code for lessons 3 and 4

---
+++ Module2/Lesson3/create-policy.sh
@@ -0,0 +1,1 @@
+az policy definition create --name LinuxPasswordPolicy --rules azurepolicy.rules.json
---
+++ Module2/Lesson4/Deploy a Linux Server in Azure/variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "username" {
+  description = "Username of the VM's OS."
+}
+variable "password" {
+  description = "Password of the VM's OS."
+}
